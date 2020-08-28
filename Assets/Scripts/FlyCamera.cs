6fdf8a46f788b5e37ff15f327d460dde42e8144a
rice_without_ice <374766941@qq.com>
2020-08-28 18:16:06 +0800

    #1. first commit

---
+++ Assets/Scripts/FlyCamera.cs
@@ -0,0 +1,502 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace YoukiaEngine
+{
+    public class FlyCamera : MonoBehaviour
+    {
+        private const float MaxEffectiveTPF = 1.0f;
+        // 是否在缩放
+        private bool bPinch = false;
+        // 是否在移动
+        private bool bMove = false;
+        // 是否在旋转
+        private bool bRotate = false;
+
+        protected enum FlyCameraAction
+        {
+            //Rotation
+            Left,
+            Right,
+            Up,
+            Down,
+            Drag,
+
+            //Translation
+            Forward,
+            Backword,
+            Leftword,
+            Rightword,
+            Rise,
+            Lower,
+
+            //Zoom
+            ZoomIn,
+            ZoomOut,
+
+            Count
+        }
+
+        //public float minX = -412.5f;
+        //public float maxX = 712.5f;
+        //public float minY = 150;
+        //public float maxY = 450;
+        //public float minZ = -412.5f;
+        //public float maxZ = 712.5f;
+
+        public float MoveSpeed = 15;
+        public float RotationSpeed = 1;
+        public float ZoomSpeed = 1;
+
+        private Vector3 _upVector;
+        private float[] _actionStatus;
+
+        private int _oldMouseX;
+        private int _oldMouseY;
+        private bool _rightMouseDown = false;
+        private bool _leftMouseDown = false;
+        private Vector2 _dragDir;
+
+        private static Transform _target = null;
+        //private static YKMapManager _ykMapMgr = null;
+        private bool isInitTheMap = false;
+
+        //        private Camera _camera;
+        // Use this for initialization
+        void Start()
+        {
+            //            _camera = GetComponent<Camera>();
+            //EasyTouch.On_PinchEnd += On_PinchEnd;
+            //EasyTouch.On_Pinch += On_Pinch;
+            _upVector = Vector3.up;
+            _actionStatus = new float[(int) FlyCameraAction.Count];
+        }
+
+        //public void SetTheCameraMoveFollowTerrainHeight(Transform target,YKMapManager mgr)
+        //{
+        //    _target = target;
+        //    _ykMapMgr = mgr;
+        //    isInitTheMap = true;
+        //}
+
+        public bool checkTheCameraInitMap()
+        {
+            return isInitTheMap;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+                 return;
+#endif
+
+            _keyPressed(this);
+            _keyReleased(this);
+            _mouseMoved(this, (int) Input.mousePosition.x, (int) Input.mousePosition.y);
+            _wheelScrooled(this);
+
+            float effectiveTPF = Math.Min(Time.deltaTime, MaxEffectiveTPF);
+
+            if (_actionStatus[(int) FlyCameraAction.Left] != 0)
+            {
+                _rotate_camera(transform, -_actionStatus[(int) FlyCameraAction.Left]*RotationSpeed, Vector3.up,
+                    _upVector);
+                _actionStatus[(int) FlyCameraAction.Left] = 0;
+            }
+            if (_actionStatus[(int) FlyCameraAction.Right] != 0)
+            {
+                _rotate_camera(transform, _actionStatus[(int) FlyCameraAction.Right]*RotationSpeed, Vector3.up,
+                    _upVector);
+                _actionStatus[(int) FlyCameraAction.Right] = 0;
+            }
+            if (_actionStatus[(int) FlyCameraAction.Up] != 0)
+            {
+                Vector3 camDir = transform.forward;
+                float dotvy = Vector3.Dot(Vector3.up, camDir);
+                Vector3 axis = Vector3.Cross(camDir, _upVector).normalized;
+                if (dotvy > -0.99f)
+                {
+                    _rotate_camera(transform, -_actionStatus[(int) FlyCameraAction.Up]*RotationSpeed, axis, _upVector);
+                }
+                _actionStatus[(int) FlyCameraAction.Up] = 0;
+            }
+            if (_actionStatus[(int) FlyCameraAction.Down] != 0)
+            {
+                Vector3 camDir = transform.forward;
+                float dotvy = Vector3.Dot(Vector3.up, camDir);
+                Vector3 axis = Vector3.Cross(camDir, _upVector).normalized;
+                if (dotvy < 0.99f)
+                {
+                    _rotate_camera(transform, _actionStatus[(int) FlyCameraAction.Down]*RotationSpeed, axis, _upVector);
+                }
+                _actionStatus[(int) FlyCameraAction.Down] = 0;
+            }
+            if (_actionStatus[(int) FlyCameraAction.Drag] != 0)
+            {
+                Vector3 direction = new Vector3(_dragDir.x, 0, _dragDir.y);
+                _move_camera(transform, _actionStatus[(int) FlyCameraAction.Drag]*MoveSpeed*effectiveTPF, direction);
+
+                _actionStatus[(int) FlyCameraAction.Drag] = 0;
+            }
+            if (_actionStatus[(int) FlyCameraAction.Forward] != 0)
+            {
+                Vector3 direction = transform.forward;
+                _move_camera(transform, _actionStatus[(int) FlyCameraAction.Forward]*MoveSpeed*effectiveTPF, direction);
+            }
+            if (_actionStatus[(int) FlyCameraAction.Backword] != 0)
+            {
+                Vector3 direction = transform.forward;
+                _move_camera(transform, -_actionStatus[(int) FlyCameraAction.Backword]*MoveSpeed*effectiveTPF, direction);
+            }
+            if (_actionStatus[(int) FlyCameraAction.Leftword] != 0)
+            {
+                Vector3 direction = transform.forward;
+                Vector3 up = transform.up;
+                Vector3 left = Vector3.Cross(up, direction);
+                _move_camera(transform, -_actionStatus[(int) FlyCameraAction.Leftword]*MoveSpeed*effectiveTPF, left);
+            }
+            if (_actionStatus[(int) FlyCameraAction.Rightword] != 0)
+            {
+                Vector3 direction = transform.forward;
+                Vector3 up = transform.up;
+                Vector3 left = Vector3.Cross(up, direction);
+                _move_camera(transform, _actionStatus[(int) FlyCameraAction.Rightword]*MoveSpeed*effectiveTPF, left);
+            }
+            if (_actionStatus[(int) FlyCameraAction.Rise] != 0)
+            {
+                Vector3 up = Vector3.up;
+                _move_camera(transform, _actionStatus[(int) FlyCameraAction.Rise]*MoveSpeed*effectiveTPF, up);
+            }
+            if (_actionStatus[(int) FlyCameraAction.Lower] != 0)
+            {
+                Vector3 up = Vector3.up;
+                _move_camera(transform, -_actionStatus[(int) FlyCameraAction.Lower]*MoveSpeed*effectiveTPF, up);
+            }
+            if (_actionStatus[(int) FlyCameraAction.ZoomIn] != 0)
+            {
+                _zoom_camera(transform, -_actionStatus[(int) FlyCameraAction.ZoomIn]*ZoomSpeed);
+                _actionStatus[(int) FlyCameraAction.ZoomIn] = 0;
+            }
+            if (_actionStatus[(int) FlyCameraAction.ZoomOut] != 0)
+            {
+                _zoom_camera(transform, _actionStatus[(int) FlyCameraAction.ZoomOut]*ZoomSpeed);
+                _actionStatus[(int) FlyCameraAction.ZoomOut] = 0;
+            }
+            //transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), Mathf.Clamp(transform.position.y, minY, maxY), Mathf.Clamp(transform.position.z, minZ, maxZ));
+        }
+
+        static void _rotate_camera(Transform cam, float value, Vector3 axis, Vector3 up)
+        {
+            Quaternion quatSrc = cam.rotation;
+            Quaternion quat = Quaternion.AngleAxis(value, axis);
+            quat = quat*quatSrc;
+            cam.rotation = quat;
+            Vector3 direction = cam.forward;
+            Vector3 left = Vector3.Cross(up, direction).normalized;
+            Vector3 fixedUp = Vector3.Cross(direction, left);
+            cam.up = fixedUp;
+            cam.forward = direction;
+        }
+
+        static void _move_camera(Transform cam, float value, Vector3 axis)
+        {
+            Vector3 vec;
+            Vector3 vecSrc = cam.position;
+            //if(YKApplication.Instance)
+            //{
+            //    vec = axis * (value / YKApplication.Instance.TimeScale);
+            //}
+            //else
+            //{
+            vec = axis*value;
+            //}
+
+            if (_target != null)
+            {
+                Vector3 pos = _target.position;
+                pos.x += vec.x;
+                pos.z += vec.z;
+                //float posY = YKMapManager.Instance.GetCurrentPosY(pos);
+                //if (posY > 0.00001f)
+                //{
+                //    vec.y += (posY - pos.y);
+                //    pos.y = posY;
+                //}
+                _target.position = pos;
+
+            }
+            vec += vecSrc;
+            cam.position = vec;
+            // YKMapManager.Instance.SetTheCurrentPlayerPos();
+        }
+
+        //void On_Pinch(Gesture gesture)
+        //{
+        //    if (bRotate || bMove)
+        //    {
+        //        return;
+        //    }
+        //    bPinch = true;
+        //    _zoom_camera(transform, gesture.deltaPinch * Time.deltaTime * ZoomSpeed * 0.01f);
+        //}
+
+        //void On_PinchEnd(Gesture gesture)
+        //{
+        //    bPinch = false;
+        //}
+
+        /// <summary>
+        /// 摇杆函数
+        /// </summary>
+        /// <param name="direction"></param>
+        public void JoyStickControlMove(Vector2 direction)
+        {
+            if (bPinch)
+            {
+                return;
+            }
+            // 横向移动
+            Vector3 v3direction = transform.forward;
+            Vector3 v3up = transform.up;
+            Vector3 v3left = Vector3.Cross(v3up, v3direction);
+            _move_camera(transform, direction.x*MoveSpeed*0.04f, v3left);
+
+            // 前进后退
+            _move_camera(transform, direction.y*MoveSpeed*0.04f, transform.forward);
+            bMove = true;
+        }
+
+        public void On_MoveEnd()
+        {
+            bMove = false;
+        }
+
+        /// <summary>
+        /// 摇杆函数
+        /// </summary>
+        /// <param name="direction"></param>
+        public void JoyStickControlRotate(Vector2 direction)
+        {
+            if (bPinch)
+            {
+                return;
+            }
+            // 左右旋转
+            _rotate_camera(transform, direction.x*RotationSpeed*0.5f, Vector3.up, _upVector);
+
+            // 上下旋转
+            Vector3 camDir = transform.forward;
+            float dotvy = Vector3.Dot(Vector3.up, camDir);
+            Vector3 axis = Vector3.Cross(camDir, _upVector).normalized;
+            if (direction.y > 0)
+            {
+                if (dotvy < 0.99f)
+                {
+                    _rotate_camera(transform, direction.y*RotationSpeed*0.5f, axis, _upVector);
+                }
+            }
+            else
+            {
+                if (dotvy > -0.99f)
+                {
+                    _rotate_camera(transform, direction.y*RotationSpeed, axis, _upVector);
+                }
+            }
+            bRotate = true;
+        }
+
+        public void On_RotateEnd()
+        {
+            bRotate = false;
+        }
+
+        static void _zoom_camera(Transform cam, float value)
+        {
+            Camera c = cam.GetComponent<Camera>();
+            if (!c)
+            {
+                return;
+            }
+
+            float zoom = 1.0f + 1.0f/5.0f*value;
+            if (c.orthographic)
+            {
+                c.orthographicSize = c.orthographicSize*zoom;
+            }
+            else
+            {
+                c.transform.position += -c.transform.forward*value*c.farClipPlane*0.1f;
+                //c.fieldOfView = c.fieldOfView * zoom;
+            }
+        }
+
+        static void _flyCameraAction(FlyCamera flyCam, FlyCameraAction action, float value)
+        {
+            flyCam._actionStatus[(int) action] = value;
+        }
+
+        private void _keyPressed(FlyCamera flyCam)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.Forward, 1);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.Backword, 1);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.Leftword, 1);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.Rightword, 1);
+            }
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.Lower, 1);
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.Rise, 1);
+            }
+        }
+
+        private void _keyReleased(FlyCamera flyCam)
+        {
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.Forward, 0);
+            }
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.Backword, 0);
+            }
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.Leftword, 0);
+            }
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.Rightword, 0);
+            }
+            if (Input.GetKeyUp(KeyCode.Q))
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.Lower, 0);
+            }
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.Rise, 0);
+            }
+        }
+
+        static void _mouseMoved(FlyCamera flyCam, int x, int y)
+        {
+            if (Input.touchCount >= 1)
+            {
+                if (Input.touches[0].phase == TouchPhase.Began)
+                {
+                    flyCam._leftMouseDown = true;
+                    flyCam._oldMouseX = x;
+                    flyCam._oldMouseY = y;
+
+                }
+                if (Input.touches[0].phase == TouchPhase.Ended)
+                {
+                    flyCam._leftMouseDown = false;
+                }
+                if (Input.touches[0].phase == TouchPhase.Moved)
+                {
+                    flyCam._oldMouseX = x;
+                    flyCam._oldMouseY = y;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+                //if (Input.GetMouseButtonDown(0))
+            {
+                flyCam._rightMouseDown = true;
+                flyCam._oldMouseX = x;
+                flyCam._oldMouseY = y;
+            }
+            if (Input.GetKeyUp(KeyCode.Mouse1))
+                // if (Input.GetMouseButtonUp(0))
+            {
+                flyCam._rightMouseDown = false;
+            }
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+                //if (Input.GetMouseButtonDown(0))
+            {
+                flyCam._leftMouseDown = true;
+                flyCam._oldMouseX = x;
+                flyCam._oldMouseY = y;
+            }
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+                // if (Input.GetMouseButtonUp(0))
+            {
+                flyCam._leftMouseDown = false;
+            }
+            float aspect = 1; //flyCam._camera.aspect;
+            if (flyCam._rightMouseDown && (flyCam._oldMouseX != x || flyCam._oldMouseY != y))
+            {
+                int offsetX = x - flyCam._oldMouseX;
+                int offsetY = y - flyCam._oldMouseY;
+                if (offsetX > 0)
+                {
+                    _flyCameraAction(flyCam, FlyCameraAction.Right, offsetX/10.0f);
+                }
+                else
+                {
+                    _flyCameraAction(flyCam, FlyCameraAction.Left, -offsetX/10.0f);
+                }
+                if (offsetY > 0)
+                {
+                    _flyCameraAction(flyCam, FlyCameraAction.Down, offsetY/10.0f/aspect);
+                }
+                else
+                {
+                    _flyCameraAction(flyCam, FlyCameraAction.Up, -offsetY/10.0f/aspect);
+                }
+            }
+
+            if (flyCam._leftMouseDown && (flyCam._oldMouseX != x || flyCam._oldMouseY != y))
+            {
+                int offsetX = x - flyCam._oldMouseX;
+                int offsetY = y - flyCam._oldMouseY;
+                Vector3 dir = new Vector3(offsetX, 0, offsetY);
+                float len = dir.magnitude;
+                dir.Normalize();
+
+                Vector3 horizontalCamDir =
+                    new Vector3(flyCam.transform.forward.x, 0, flyCam.transform.forward.z).normalized;
+                Vector3 originalDir = new Vector3(0, 0, 1);
+                Quaternion q = Quaternion.FromToRotation(originalDir, horizontalCamDir);
+                Vector3 tmp = -(q*dir);
+                flyCam._dragDir = new Vector2(tmp.x, tmp.z);
+
+                _flyCameraAction(flyCam, FlyCameraAction.Drag, len*0.2f);
+            }
+            flyCam._oldMouseX = x;
+            flyCam._oldMouseY = y;
+        }
+
+        static void _wheelScrooled(FlyCamera flyCam)
+        {
+            float amount = Input.GetAxis("Mouse ScrollWheel");
+
+            if (amount > 0)
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.ZoomIn, (float) amount);
+            }
+            else if (amount < 0)
+            {
+                _flyCameraAction(flyCam, FlyCameraAction.ZoomOut, -(float) amount);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RayTracingMaster.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTracingMaster : MonoBehaviour
+{
+	public struct Sphere
+	{
+		public Vector3 pos;
+		// r
+		public float r;
+		public Vector3 albedo;
+		public Vector3 spec;
+	};
+
+	private const float ThreadUnit = 8.0f;
+
+	[Header("Light")]
+	public Light DirLight;
+	[Header("Shaders")]
+	public ComputeShader RayTracingShader;
+	public Shader AccShader;
+	[Space(5)]
+	public Texture SkyboxTexture;
+	[Header("Sphere")]
+	public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
+	public uint SphereMax = 100;
+	public float SpherePlacementRadius = 100.0f;
+
+	private RenderTexture _rtTarget;
+
+	// cmp shader
+	private int _kernelMain;
+	private int _threadX;
+	private int _threadY;
+
+	// acc
+	private int _curSample = 0;
+	private Material _matAcc;
+
+	// cam
+	private Camera _camera;
+
+	// sphere
+	private ComputeBuffer _sphereBuffer;
+
+	private void Start()
+	{
+		Init();
+	}
+
+	private void Init()
+	{
+		_camera = GetComponent<Camera>();
+		InitRenderTexture();
+		InitComputeShader();
+		InitAccMat();
+		InitSpheres();
+	}
+
+	private void InitRenderTexture()
+	{
+		if (_rtTarget == null)
+		{
+			_rtTarget = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+			_rtTarget.enableRandomWrite = true;
+			_rtTarget.Create();
+		}
+	}
+
+	private void InitComputeShader()
+	{
+		if (RayTracingShader)
+		{
+			_kernelMain = RayTracingShader.FindKernel("CSMain");
+		}
+		_threadX = Mathf.CeilToInt(Screen.width / ThreadUnit);
+		_threadY = Mathf.CeilToInt(Screen.height / ThreadUnit);
+	}
+
+	private void InitAccMat()
+	{
+		if (_matAcc == null)
+		{
+			_matAcc = new Material(AccShader);
+		}
+	}
+
+	private void InitSpheres()
+	{
+		List<Sphere> spheres = new List<Sphere>();
+
+		for (int i = 0; i < SphereMax; i++)
+		{
+			Sphere s = new Sphere();
+
+			s.r = SphereRadius.x + Random.value * (SphereRadius.y - SphereRadius.x);
+			Vector2 pos = Random.insideUnitCircle * SpherePlacementRadius;
+			s.pos = new Vector3(pos.x, s.r, pos.y);
+
+			foreach (var other in spheres)
+			{
+				float minDis = s.r + other.r;
+				if (Vector3.SqrMagnitude(s.pos - other.pos) < minDis * minDis)
+					goto SkipSphere;
+			}
+
+			Color col = Random.ColorHSV();
+			bool metal = Random.value < 0.5f;
+			s.albedo = metal ? Vector3.zero : new Vector3(col.r, col.g, col.b);
+			s.spec = metal ? new Vector3(col.r, col.g, col.b) : Vector3.one * 0.04f;
+
+			spheres.Add(s);
+
+		SkipSphere:
+				continue;
+		}
+
+		_sphereBuffer = new ComputeBuffer(spheres.Count, 40);
+		_sphereBuffer.SetData(spheres);
+	}
+
+	private void Update()
+	{
+		if (transform.hasChanged)
+		{
+			_curSample = 0;
+			transform.hasChanged = false;
+		}
+	}
+
+	private void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		if (_rtTarget == null || RayTracingShader == null || _camera == null || _matAcc == null)
+			Graphics.Blit(src, dest);
+		else
+		{
+			Render(dest);
+		}
+	}
+
+	private void Render(RenderTexture dest)
+	{
+		SetShaderParameters();
+		RayTracingShader.SetTexture(_kernelMain, "Result", _rtTarget);
+		// sky box
+		RayTracingShader.SetTexture(_kernelMain, "_SkyboxTex", SkyboxTexture);
+		RayTracingShader.Dispatch(_kernelMain, _threadX, _threadY, 1);
+		// Graphics.Blit(_rtTarget, dest);
+
+		// acc
+		_matAcc.SetFloat("_Sample", _curSample);
+		Graphics.Blit(_rtTarget, dest, _matAcc);
+		_curSample++;
+	}
+
+	private void SetShaderParameters()
+	{
+		RayTracingShader.SetMatrix("_C2W", _camera.cameraToWorldMatrix);
+		RayTracingShader.SetMatrix("_IP", _camera.projectionMatrix.inverse);
+		RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
+		// light
+		Vector3 lightDir = DirLight.transform.forward;
+		RayTracingShader.SetVector("_DirectionalLight", new Vector4(lightDir.x, lightDir.y, lightDir.z, DirLight.intensity));
+		// spheres
+		RayTracingShader.SetBuffer(_kernelMain, "_Spheres", _sphereBuffer);
+	}
+
+	private void OnDisable()
+	{
+		OnRelease();
+	}
+
+	private void OnRelease()
+	{
+		if (_rtTarget != null)
+		{
+			_rtTarget.Release();
+			GameObject.Destroy(_rtTarget);
+		}
+	}
+}
